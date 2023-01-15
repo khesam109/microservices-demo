24613e72c1df302894b2a9cb24e58b2843c053b3
khesam109 <khesam109@gmail.com>
2023-01-15 05:28:49 -0800

    add kafka admin client

---
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/exception/KafkaClientException.java
@@ -0,0 +1,12 @@
+package com.microservices.demo.kafka.admin.exception;
+
+public class KafkaClientException extends RuntimeException {
+
+    public KafkaClientException(String message) {
+        super(message);
+    }
+
+    public KafkaClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
