3567eb984e7bc613625623154d5a75134e51a324
khesam109 <khesam109@gmail.com>
2023-02-09 07:31:03 -0800

    add elastic config

---
+++ elastic/elastic-config/src/main/java/com/microservices/demo/elastic/config/ElasticSearchConfig.java
@@ -0,0 +1,44 @@
+package com.microservices.demo.elastic.config;
+
+import com.microservices.demo.config.ElasticConfigData;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.erhlc.AbstractElasticsearchConfiguration;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Objects;
+
+@Configuration
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    private final ElasticConfigData elasticConfigData;
+
+    @Autowired
+    public ElasticSearchConfig(ElasticConfigData elasticConfigData) {
+        this.elasticConfigData = elasticConfigData;
+    }
+
+    @Bean
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        UriComponents serverUri = UriComponentsBuilder.fromHttpUrl(elasticConfigData.getConnectionUrl()).build();
+        return new RestHighLevelClient(
+                RestClient.builder(new HttpHost(
+                        Objects.requireNonNull(serverUri.getHost()),
+                        serverUri.getPort(),
+                        serverUri.getScheme()
+                )).setRequestConfigCallback(
+                        requestConfigBuilder ->
+                                requestConfigBuilder
+                                        .setConnectTimeout(elasticConfigData.getConnectionTimeout())
+                                        .setSocketTimeout(elasticConfigData.getSocketTimeout())
+
+                )
+        );
+    }
+}
