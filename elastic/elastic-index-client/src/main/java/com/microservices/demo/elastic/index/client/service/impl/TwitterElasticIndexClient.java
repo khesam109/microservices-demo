66e1708c360e8dfeeab690f0a71b1cce1339be88
khesam109 <khesam109@gmail.com>
2023-02-10 00:23:48 -0800

    elastic index impl

---
+++ elastic/elastic-index-client/src/main/java/com/microservices/demo/elastic/index/client/service/impl/TwitterElasticIndexClient.java
@@ -0,0 +1,47 @@
+package com.microservices.demo.elastic.index.client.service.impl;
+
+import com.microservices.demo.config.ElasticConfigData;
+import com.microservices.demo.elastic.index.client.service.ElasticIndexClient;
+import com.microservices.demo.elastic.index.client.util.ElasticIndexUtil;
+import com.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexedObjectInformation;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TwitterElasticIndexClient implements ElasticIndexClient<TwitterIndexModel> {
+
+    private final static Logger LOG = LoggerFactory.getLogger(TwitterElasticIndexClient.class);
+
+    private final ElasticConfigData elasticConfigData;
+    private final ElasticsearchOperations elasticsearchOperations;
+    private final ElasticIndexUtil<TwitterIndexModel> elasticIndexUtil;
+    @Autowired
+    public TwitterElasticIndexClient(
+            ElasticConfigData elasticConfigData,
+            ElasticsearchOperations elasticsearchOperations,
+            ElasticIndexUtil<TwitterIndexModel> elasticIndexUtil
+    ) {
+        this.elasticConfigData = elasticConfigData;
+        this.elasticsearchOperations = elasticsearchOperations;
+        this.elasticIndexUtil = elasticIndexUtil;
+    }
+
+    @Override
+    public List<String> save(List<TwitterIndexModel> documents) {
+        List<IndexQuery> indexQueries = elasticIndexUtil.getIndexQueries(documents);
+        return elasticsearchOperations.bulkIndex(
+                indexQueries, IndexCoordinates.of(
+                        elasticConfigData.getIndexName()
+                )
+        ).stream().map(IndexedObjectInformation::getId).toList();
+    }
+}
